8d5e420900d86b3c217f649699fd82aac25a8e14
Jheniffer Daniely <54866199+userdajheni@users.noreply.github.com>
2023-06-21 11:13:36 -0300

    Exibir maior numero

---
+++ dev-aprender/exercicio4.js
@@ -0,0 +1,11 @@
+function maior (num1, num2) {
+    if (num1 > num2) {
+        console.log('O primeiro número é maior')
+    } else if (num1 == num2) {
+        console.log('Os numeros são iguais')
+    } else {
+        console.log('O segundo numero é maior')
+    }
+}
+
+maior(5,26)
