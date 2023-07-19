336cd26be1eba2ae1ad8d69ca61f16a9a95742bb
Jheniffer Daniely <54866199+userdajheni@users.noreply.github.com>
2023-07-19 11:28:44 -0300

    função exibe se cada valor é par ou impar

---
+++ dev-aprender/exercicio8.js
@@ -0,0 +1,13 @@
+// receber uma quantidade de valores para avaliar
+// função exibe se cada valor é par ou impar
+
+exibirTipo(10)
+function exibirTipo(limite){
+    for (let i = 0; i <= limite; i++){
+        if(i % 2 == 0){
+            console.log(i,'PAR')
+        } else {
+            console.log(i,'IMPAR')
+        }
+    }
+}
