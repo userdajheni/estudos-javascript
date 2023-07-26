d0a99096f9b3169f4e1e16df8b5ff3a7b05c19e7
Jheniffer Daniely <54866199+userdajheni@users.noreply.github.com>
2023-07-26 13:37:59 -0300

    Objetos

---
+++ w3school/ex12.js
@@ -0,0 +1,10 @@
+const pessoa = {
+    nome : 'ana',
+    sobrenome : 'silva',
+    idade : 25,
+    corOlhos : 'castanho'
+}
+
+console.log(pessoa) // saída: { nome: 'ana', sobrenome: 'silva', idade: 25, corOlhos: 'castanho' }
+
+console.log(pessoa.nome) //saída: ana
