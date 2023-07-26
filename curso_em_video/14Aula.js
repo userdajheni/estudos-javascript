245147a55669149c18676ad63c4be08f41728a38
Jheniffer Daniely <54866199+userdajheni@users.noreply.github.com>
2023-07-26 08:13:14 -0300

    Loop For

---
+++ curso_em_video/14Aula.js
@@ -0,0 +1,18 @@
+let contador = 1
+
+while (contador < 6) {
+    console.log('Tudo bem? '+contador)
+    contador++
+}
+
+let cont = 10
+while (cont >= 0) {
+    console.log(`Contagem regressiva ${cont}`)
+    cont--
+}
+
+let c = 5
+do {
+    console.log('Meu aniversário está chegando! ' + `(${c})`)
+    c++
+} while (c < 15)
---
+++ curso_em_video/15Aula.js
@@ -0,0 +1,9 @@
+// for(inicio; teste lógico; incremento)
+for (let cont = 0; cont <= 10; cont++) {
+    console.log(cont)
+}
+
+for (let reg = 10; reg > 0; reg--){
+    console.log(`Contagem regressiva ${reg}`)
+}
+console.log('Fim da contagem regressiva')
