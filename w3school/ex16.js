e879fa4597254ef70c754b83a544a335c110b915
Jheniffer Daniely <54866199+userdajheni@users.noreply.github.com>
2023-07-27 10:37:52 -0300

    Modelo Literais / Modelos de String / Back-Tics

---
+++ w3school/ex16.js
@@ -0,0 +1,15 @@
+// console.log("testando aspas dentro de string "ok""). Isso não pode. Da erro!
+
+console.log(`Testando "aspas" dentro de 'uma' string`)
+
+let texto = `Com o modelo literal
+podemos escrever uma string
+com várias linhas sem 
+nenhum problema`
+
+console.log(texto)
+
+let a = 5
+let b = 8
+
+console.log(`A soma entre ${a} e ${b} é igual a: ${a+b}`)
