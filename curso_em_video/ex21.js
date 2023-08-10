1ca0d19060a49256354ccfad1ce6ac8d229c710f
Jheniffer Daniely <54866199+userdajheni@users.noreply.github.com>
2023-08-10 14:08:22 -0300

    delete

--- curso_em_video/ex21.js
+++
@@ -1,9 +0,0 @@
-// use sort() para ordenar alfabeticamente um array
-let frutas = ['Banana', 'Maçã', 'Uva', 'Amora', 'Framboesa']
-console.log('Array sem sort: ', frutas)
-
-frutas.sort()
-console.log('Array com sort: ',frutas)
-
-frutas.reverse()
-console.log(`Invertendo um array: ${frutas}`)  
